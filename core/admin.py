02aed05b3c0367629972747b9a66f4f4b3e547ab
pathum lakshan <123624162+pathumlak@users.noreply.github.com>
2026-09-07 07:30:30 +0530

    hi

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import User
 
-
+# hi
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     list_display = ("username", "email", "role", "is_staff", "is_active")
